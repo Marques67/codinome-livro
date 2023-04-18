33a3f7f65c163d0623c6a780f2126b7b60814146
lucasma2009_00 <lucas.marques@eurekalabs.com.br>
2023-04-18 20:08:44 -0300

    Backend project created

---
+++ backend/src/main/java/br/com/codinomelivros/CodinomeLivrosApplication.java
@@ -0,0 +1,13 @@
+package br.com.codinomelivros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodinomeLivrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodinomeLivrosApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/codinomelivros/CodinomeLivrosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.codinomelivros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodinomeLivrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
